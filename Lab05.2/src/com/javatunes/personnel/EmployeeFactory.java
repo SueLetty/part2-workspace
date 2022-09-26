2b8ed5c1bc50a0dfd4c7eed132f2d57b95a54d88
SueLetty <xiuxiang1129@gmail.com>
2022-09-26 15:00:13 -0700

    Completed Lab05.2

--- Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
+++ Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
@@ -12,20 +12,37 @@ import java.sql.Date;
 import java.util.Map;
 
 public class EmployeeFactory {
-  
+
   // prevent direct instantiation - this is an all-static factory class
   private EmployeeFactory() {
   }
-  
+
   /**
    * TODO: given the input map, create and return the correct object (with its properties set).
    * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
    */
-  public static Employee createEmployee(Map<String,String> inputMap)
-  throws IllegalArgumentException {
-    // return value
+  public static Employee createEmployee(Map<String, String> inputMap)
+      throws IllegalArgumentException {
+
     Employee emp = null;
-    
+    String type = inputMap.get("type");
+
+    if (!(type.equals("HE") || type.equals("SE"))) {
+      throw new IllegalArgumentException("It is a BAD_TYPE");
+    }
+    String name = inputMap.get("name");
+    Date hireDate = Date.valueOf(inputMap.get("hireDate"));
+
+    if (type.equals("HE")) {
+      Double rate = Double.valueOf(inputMap.get("rate"));
+      Double hours = Double.valueOf(inputMap.get("hours"));
+      emp = new HourlyEmployee(name, hireDate, rate, hours);
+    } else {
+      Double salary = Double.valueOf(inputMap.get("salary"));
+      emp = new SalariedEmployee(name, hireDate, salary);
+
+    }
+
     return emp;
   }
 }
