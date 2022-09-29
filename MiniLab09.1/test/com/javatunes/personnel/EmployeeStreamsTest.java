e0234dc9816d604a3823c85343b591e7d32545d1
SueLetty <xiuxiang1129@gmail.com>
2022-09-29 10:09:00 -0700

    Completed miniLab09.1

--- MiniLab09.1/test/com/javatunes/personnel/EmployeeStreamsTest.java
+++ MiniLab09.1/test/com/javatunes/personnel/EmployeeStreamsTest.java
@@ -9,36 +9,39 @@
 package com.javatunes.personnel;
 
 import static org.junit.Assert.*;
+
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 
 public class EmployeeStreamsTest {
-  
+
   private Collection<Employee> allEmployees;
-  
+
   @Before
   public void setUp() {
     allEmployees = Department.getDepartment().getEmployees();
   }
-  
+
   /**
-   * TASK: find all Employees with salary < 40000.0 and sort them by natural order.
-   * Employee implements Comparable: natural order is id increasing.
-   * 
+   * TASK: find all Employees with salary < 40000.0 and sort them by natural order. Employee
+   * implements Comparable: natural order is id increasing.
+   * <p>
    * RESULT: Employees 2 and 15, in that order.
    */
   @Test
   public void testSalaryLessThanSortNaturalOrder() {
-    List<Employee> employees = allEmployees.stream()
-      .filter(emp -> emp.getSalary() < 40000.0)
-      .sorted()  // natural order
-      .collect(Collectors.toList());
-    
+    List<Employee> employees = allEmployees
+        .stream()
+        .filter(emp -> emp.getSalary() < 40000.0)
+        .sorted()  // natural order
+        .collect(Collectors.toList());
+
     assertEquals(2, employees.size());
-    assertEquals(Long.valueOf(2),  employees.get(0).getId());
+    assertEquals(Long.valueOf(2), employees.get(0).getId());
     assertEquals(Long.valueOf(15), employees.get(1).getId());
   }
 }
